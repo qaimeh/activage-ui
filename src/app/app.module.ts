11c76ae2087a7b61e58be4dc45a4629f2c7e3679
Qaiser Mehmood <qaiser.mehmood@deri.org>
2020-05-09 17:05:43 +0100

    bootstrap-jquery-popper installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SideBarComponent,
   ],
   imports: [
     BrowserModule,
